e75d0e52deebc3977c8a2e82e9b23a3fd5c55b5c
E Stringham <backfrip@users.noreply.github.com>
2015-07-27 13:59:20 -0400

    Ooops, forgot to commit!

--- new-work.php
+++ new-work.php
@@ -52,33 +52,67 @@
       <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
 
       <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
-        <table class="table">
-          <th>
-            <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><h5 class="sans caps light-grey">Work Items</h5></td>
-            <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><h5 class="sans caps right-align">$49.14</h5></td>
-          </th>
-          <tr>
-            <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><span class="sans">Air filter</span></td>
-            <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><span class="sans right-align">$18.00</span></td>
-          </tr>
-          <tr>
-            <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><span class="sans">Cabin air filter</span></td>
-            <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><span class="sans right-align">$36.30</span></td>
-          </tr>
-          <tr>
-            <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><span class="sans">Engine oil</span></td>
-            <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><span class="sans right-align">$36.47</span></td>
-          </tr>
-          <tr>
-            <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><span class="sans">Oil filter</span></td>
-            <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><span class="sans right-align">$6.20</span></td>
-          </tr>
+        <table class="table sans">
+          <thead>
+            <tr>
+              <th class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><h5 class="caps section-header light-grey">Work Items</h5></th>
+              <th class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align">$49.14</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8">Air filter</td>
+              <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align">$18.00</td>
+            </tr>
+            <tr>
+              <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8">Cabin air filter</td>
+              <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align">$36.30</td>
+            </tr>
+            <tr>
+              <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8">Engine oil</td>
+              <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align">$36.47</td>
+            </tr>
+            <tr>
+              <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8">Oil filter</td>
+              <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align">$6.20</td>
+            </tr>
+          </tbody>
+          </table>
+
+        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <button class="btn btn-default col-lg-2 col-md-2 col-sm-2 col-xs-2" type="button">+ Add new work item</button> <!--Not entirely sure how this is going to work, I'll throw in a button for now-->
+        </div>
+
+        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+
+        <table class="table sans">
+          <thead>
+            <tr>
+              <th class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><h5 class="caps section-header light-grey">Labor Cost</h5></th>
+              <th class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align">$35.00</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="col-lg-8 col-md-8 col-sm-8 col-xs-8">45 min of labor</td>
+              <td class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align"></td>
+            </tr>
+          </tbody>
         </table>
 
-        <button class="btn btn-default" type="button">+ Add new work item</button> <!--Not entirely sure how this is going to work, I'll throw in a button for now-->
+        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
 
-        
-        
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <h5 class="sans caps section-header light-grey">Comments</h5>
+        </div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <textarea class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          </textarea>
+        </div>
+
       </div>
 
     </div>
