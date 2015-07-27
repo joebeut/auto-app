960ef03734b4311ce232952c68ddf9e7e49536f4
E Stringham <joebeut@gmail.com>
2015-07-27 15:58:33 -0400

    Hopefully this commits from backfrip this time.

--- new-work.php
+++ new-work.php
@@ -35,14 +35,10 @@
 
       <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
 
-      <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-        <h4 class="sans caps"><a href="#"><u>New Work</u></a></h4>
-      </div>
-      <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-        <h4 class="sans caps"><a class="not-active" href="#">Existing Work</a></h4>
-      </div>
-      <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-        <h4 class="sans caps"><a class="not-active" href="#">Vehicle Information</a></h4>
+      <div class="col-lg-11 col-md-11 col-sm-11 col-xs-11">
+        <h4 class="sans caps inline"><a href="#"><u>New Work</u></a></h4>
+        <h4 class="sans caps inline"><a class="not-active" href="#">Existing Work</a></h4>
+        <h4 class="sans caps inline"><a class="not-active" href="#">Vehicle Information</a></h4>
       </div>
 
     </div>
@@ -55,7 +51,7 @@
         <table class="table sans">
           <thead>
             <tr>
-              <th class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><h5 class="caps section-header light-grey">Work Items</h5></th>
+              <th class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><span class="sans caps light-grey vbottom table-header">Work Items</span></th>
               <th class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align">$49.14</th>
             </tr>
           </thead>
@@ -90,7 +86,7 @@
         <table class="table sans">
           <thead>
             <tr>
-              <th class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><h5 class="caps section-header light-grey">Labor Cost</h5></th>
+              <th class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><span class="sans caps light-grey vbottom table-header">Labor Cost</span></th>
               <th class="col-lg-4 col-md-4 col-sm-4 col-xs-4 right-align">$35.00</th>
             </tr>
           </thead>
@@ -105,12 +101,20 @@
         <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
 
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <h5 class="sans caps section-header light-grey">Comments</h5>
+          <span class="sans caps light-grey table-header">Comments</span>
         </div>
 
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <textarea class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          </textarea>
+          <textarea class="col-lg-12 col-md-12 col-sm-12 col-xs-12" rows="7"></textarea>
+        </div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <button class="btn btn-default col-lg-2 col-md-2 col-sm-2 col-xs-2" type="button">Save</button> <!--Not entirely sure how this is going to work, I'll throw in a button for now-->
+
+          <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 right-align">
+            <h5 class="sans caps light-grey table-header">Total Cost</h5>
+            <span class="sans bold">$84.14</span>
+          </div>
         </div>
 
       </div>
